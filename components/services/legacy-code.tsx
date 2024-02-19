bb5dbd0938cdc898d7bbaf3ea0febc7088c247ae
Fred Banda <fredcbanda@hotmail.com>
2024-02-19 17:47:58 +0200

    added content2

--- components/services/legacy-code.tsx
+++ components/services/legacy-code.tsx
@@ -9,12 +9,12 @@ const LegacyCode = () => {
         <div className="bg-slate-700 md:shadow-md rounded-xl m-16 border border-indigo-500/50 shadow-xl shadow-indigo-500/50">
           <div className="flex flex-col p-10 px-16 space-y-6 items-center text-center">
             <h1 className="font-light md:text-6xl text-5xl text-white tracking-wide ">
-              Hire the <span id="spin" className="text-transparent font-normal bg-clip-text bg-gradient-to-r from-indigo-400 to-cyan-300 "></span> <br /> expert you need
+              The best <span id="spin" className="text-transparent font-normal bg-clip-text bg-gradient-to-r from-indigo-400 to-cyan-300 "></span> <br /> We do that better
             </h1>
             <p className="text-gray-400 md:text-2xl text-xl px-18">
-              Create a job to connect with freelancers and agencies who can increase your sales, make your website stand out, and more
+              Sometimes your business don&apos;t need a full time hire. Give us to fix your software issues and see your income flowing. Digital world is changing so fast don&apos;t be left behind.
             </p>
-            <button className="rounded-full bg-indigo-500 shadow-lg shadow-indigo-500/50 text-white text-lg py-4 px-6">Find Expert</button>
+            <button className="rounded-full bg-indigo-500 shadow-lg shadow-indigo-500/50 text-white text-lg py-4 px-6">Book A Call</button>
           </div>
         </div>
       </div>
--- components/services/service-list.tsx
+++ components/services/service-list.tsx
@@ -15,8 +15,7 @@ const ServiceList = () => {
                             d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
                     </svg></span>
                 <p className="text-xl font-medium text-neutral-100 mt-3">Most Experienced Team</p>
-                <p className="mt-2 text-sm text-neutral-100">Team BrainEdge education is a bunch of highly focused, energetic
-                    set of people.</p>
+                <p className="mt-2 text-sm text-neutral-100">We are a dream team when it comes to work ethics. We always finish what we start ahead of time. Let professionals do the work to save you money.</p>
             </div>
 
             <div
@@ -30,9 +29,8 @@ const ServiceList = () => {
                         <line x1="16" y1="17" x2="8" y2="17"></line>
                         <polyline points="10 9 9 9 8 9"></polyline>
                     </svg></span>
-                <p className="text-xl font-medium text-neutral-100 mt-3">Best
-                    Test preparation</p>
-                <p className="mt-2 text-sm text-neutral-100 ">Know where you stand and what next to do to succeed .</p>
+                <p className="text-xl font-medium text-neutral-100 mt-3">Cobol Legacy Code</p>
+                <p className="mt-2 text-sm text-neutral-100 ">We are the best Cobol developers in the world. We can set your application to perform like never before. Give us a call today.</p>
             </div>
 
             <div className="p-10 flex flex-col items-center text-center group   md:lg:xl:border-b hover:bg-slate-500 cursor-pointer">
@@ -42,9 +40,8 @@ const ServiceList = () => {
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
                     </svg></span>
-                <p className="text-xl font-medium text-neutral-100 mt-3">Admission process Guidance</p>
-                <p className="mt-2 text-sm text-neutral-100">Professional Advice for higher education abroad and select the
-                    top institutions worldwide.</p>
+                <p className="text-xl font-medium text-neutral-100 mt-3">Internet Security</p>
+                <p className="mt-2 text-sm text-neutral-100">Well all crooks are living on the internet now. Are your applications really secure from breaks? Let us help you out.</p>
             </div>
 
 
@@ -54,9 +51,8 @@ const ServiceList = () => {
                         stroke="currentColor" stroke-width="1.5">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />
                     </svg></span>
-                <p className="text-xl font-medium text-neutral-100 mt-3">Best
-                    Track Record</p>
-                <p className="mt-2 text-sm text-neutral-100">Yet another year ! Yet another jewel in our crown!</p>
+                <p className="text-xl font-medium text-neutral-100 mt-3">Shopify Stores</p>
+                <p className="mt-2 text-sm text-neutral-100">Ever heard of dropshipping or you want to sell your products? Shopify is always a great choice. They have an already proven formulae that work.</p>
             </div>
 
             <div className="p-10 flex flex-col items-center text-center group    md:lg:xl:border-r hover:bg-slate-500 cursor-pointer">
@@ -66,10 +62,8 @@ const ServiceList = () => {
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
                     </svg></span>
-                <p className="text-xl font-medium text-neutral-100 mt-3">Free
-                    Mock Exams</p>
-                <p className="mt-2 text-sm text-neutral-100">Get Topic-Wise Tests, Section- Wise and mock tests for your
-                    preparation.</p>
+                <p className="text-xl font-medium text-neutral-100 mt-3">Greenfield Projects</p>
+                <p className="mt-2 text-sm text-neutral-100">You have an Idea for an application that will change the world? Well you need people like us who can breathe life into that idea</p>
             </div>
 
             <div className="p-10 flex flex-col items-center text-center group hover:bg-slate-500 cursor-pointer">
@@ -79,9 +73,8 @@ const ServiceList = () => {
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
                     </svg></span>
-                <p className="text-xl font-medium text-neutral-100 mt-3">Genuine
-                    Visa Advice</p>
-                <p className="mt-2 text-sm text-neutral-100">Visa process by helping you create the necessary documentation
+                <p className="text-xl font-medium text-neutral-100 mt-3">C# Applications</p>
+                <p className="mt-2 text-sm text-neutral-100">We have years of working with C# applications and SQL servers so you wont go wrong with us. What are you waiting for?
                 </p>
             </div>
 
@@ -92,7 +85,7 @@ const ServiceList = () => {
 
         <div className="w-full   bg-indigo-600 shadow-xl shadow-indigo-200 py-10 px-20 flex flex-col justify-between items-center">
         
-            <p className=" text-white"> <span className="text-2xl font-medium items-center">Still Confused ?</span> <br /> <span className="text-lg">Book For Free Career Consultation Today ! </span></p>
+            <p className=" text-white"> <span className="text-2xl font-medium items-center">Still Confused ?</span> <br /> <span className="text-lg">Book For Free Career one on one Consultation Today ! </span></p>
             <button className="px-5 py-3 font-medium text-neutral-100 shadow-xl  hover:bg-black duration-150  bg-yellow-400 rounded-md">BOOK AN APPOINTMENT </button>
         </div>
 
