9f666942d75de654d078a642e71c959bb7d4fcab
Fred Banda <fredcbanda@hotmail.com>
2024-02-19 20:57:36 +0200

    no spotlight

--- app/page.tsx
+++ app/page.tsx
@@ -12,10 +12,6 @@ export default function Home() {
   return (
      <div className="w-full md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
         <Navbar />
-        <Spotlight
-        className="hidden md:flex md:top-80 left-80"
-        fill="white"
-        />
         <div className="p-4 mx-auto relative z-10 w-full pt-10 md-pt-18 px-2">
           <div className="text-4xl pb-5 md:text-7xl px-6 text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to bg--neutral-400 bg-opacity-50">
             Partner with us <br /> for immeasurable growth!
