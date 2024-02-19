e0a1af4befafa0d092b5c498db922813d89dfac8
Fred Banda <fredcbanda@hotmail.com>
2024-02-19 19:15:56 +0200

    corrected imports

--- app/snippets/3d-card-snippet.tsx
+++ app/snippets/3d-card-snippet.tsx
@@ -1,8 +1,8 @@
 "use client";
 
+import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
 import Image from "next/image";
 import React from "react";
-import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
 
 export function ThreeDCardExample() {
   return (
