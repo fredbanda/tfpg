350a5347bea548091f6a3b0a88a942ffd4b76caa
Fred Banda <fredcbanda@hotmail.com>
2024-02-19 17:55:02 +0200

    working

--- components/services/legacy-code.tsx
+++ components/services/legacy-code.tsx
@@ -12,7 +12,7 @@ const LegacyCode = () => {
               The best <span id="spin" className="text-transparent font-normal bg-clip-text bg-gradient-to-r from-indigo-400 to-cyan-300 "></span> <br /> we have them here.
             </h1>
             <p className="text-gray-400 md:text-2xl text-xl px-18">
-              Sometimes your business don&apos;t need a full time hire. Give us to fix your software issues and see your income flowing. Digital world is changing so fast don&apos;t be left behind.
+              Sometimes your business don&apos;t need a full time hire. Give us a chance to fix your software issues and see your income flowing. Digital world is changing so fast don&apos;t be left behind.
             </p>
             <button className="rounded-full bg-indigo-500 shadow-lg shadow-indigo-500/50 text-white text-lg py-4 px-6">Book A Call</button>
           </div>
