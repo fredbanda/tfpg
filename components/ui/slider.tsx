b4b91d7827a400159e077aee12b00e8808df8e9b
Fred Banda <fredcbanda@hotmail.com>
2024-02-19 19:20:57 +0200

    corrected imports

--- components/ui/slider.tsx
+++ components/ui/slider.tsx
@@ -1,28 +1,112 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as SliderPrimitive from "@radix-ui/react-slider"
+import Slider from "react-slick";
+import Image from "next/image";
+import { useMediaQuery } from "react-responsive";
 
-import { cn } from "@/lib/utils"
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-const Slider = React.forwardRef<
-  React.ElementRef<typeof SliderPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
->(({ className, ...props }, ref) => (
-  <SliderPrimitive.Root
-    ref={ref}
-    className={cn(
-      "relative flex w-full touch-none select-none items-center",
-      className
-    )}
-    {...props}
-  >
-    <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-secondary">
-      <SliderPrimitive.Range className="absolute h-full bg-primary" />
-    </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" />
-  </SliderPrimitive.Root>
-))
-Slider.displayName = SliderPrimitive.Root.displayName
+const SliderOne = () => {
+  const isMediumScreen = useMediaQuery({ minWidth: 768, maxWidth: 1023 });
+  const isSmallScreen = useMediaQuery({ maxWidth: 767 });
 
-export { Slider }
+  const settings = {
+    arrows: false,
+    infinite: true,
+    slidesToShow: 3,
+    autoplay: true,
+    speed: 4000,
+    autoplaySpeed: 1000,
+    className: "w-full mx-auto cursor-pointer center-mode",
+  };
+
+  if (isMediumScreen) {
+    settings.slidesToShow = 1.67;
+  } else if (isSmallScreen) {
+    settings.slidesToShow = 1;
+  }
+
+  return (
+    <div>
+      <Slider {...settings}>
+        <>
+          <div className="rounded-md px-2 md:p-10">
+            <Image
+              priority
+              src="/images/business.jpeg"
+              alt="logo"
+              width={500}
+              height={500}
+              className="
+                  rounded-2xl
+                      "
+            />
+          </div>
+        </>
+
+        <>
+          <div className="rounded-md px-2 md:p-10">
+            <Image
+              priority
+              src="/images/coffe.jpeg"
+              alt="logo"
+              width={500}
+              height={500}
+              className="
+                  rounded-2xl 
+                      "
+            />
+          </div>
+        </>
+
+        <>
+          <div className="rounded-md px-2 md:p-10">
+            <Image
+              priority
+              src="/images/small-business-3.webp"
+              alt="logo"
+              width={500}
+              height={500}
+              className="
+                  rounded-2xl 
+                      "
+            />
+          </div>
+        </>
+
+        <>
+          <div className="rounded-md px-2 md:p-10">
+            <Image
+              priority
+              src="/images/man.jpeg"
+              alt="logo"
+              width={500}
+              height={500}
+              className="
+                  rounded-2xl 
+                      "
+            />
+          </div>
+        </>
+
+        <>
+          <div className="rounded-md px-2 md:p-10">
+            <Image
+              priority
+              src="/images/image-business.jpeg"
+              alt="logo"
+              width={500}
+              height={500}
+              className="
+                  rounded-2xl 
+                      "
+            />
+          </div>
+        </>
+      </Slider>
+    </div>
+  );
+};
+
+export default SliderOne;
