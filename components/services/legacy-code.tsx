9c93cc7547fc839d40c39e8e93270fcb12f8402d
Fred Banda <fredcbanda@hotmail.com>
2024-02-19 17:51:11 +0200

    legacy

--- components/services/legacy-code.tsx
+++ components/services/legacy-code.tsx
@@ -9,7 +9,7 @@ const LegacyCode = () => {
         <div className="bg-slate-700 md:shadow-md rounded-xl m-16 border border-indigo-500/50 shadow-xl shadow-indigo-500/50">
           <div className="flex flex-col p-10 px-16 space-y-6 items-center text-center">
             <h1 className="font-light md:text-6xl text-5xl text-white tracking-wide ">
-              The best <span id="spin" className="text-transparent font-normal bg-clip-text bg-gradient-to-r from-indigo-400 to-cyan-300 "></span> <br /> We do that better
+              The best <span id="spin" className="text-transparent font-normal bg-clip-text bg-gradient-to-r from-indigo-400 to-cyan-300 "></span> <br /> we have them here.
             </h1>
             <p className="text-gray-400 md:text-2xl text-xl px-18">
               Sometimes your business don&apos;t need a full time hire. Give us to fix your software issues and see your income flowing. Digital world is changing so fast don&apos;t be left behind.
