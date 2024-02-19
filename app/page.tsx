4bc3c1428eb479becd0366ec1b0c3b7722ba33a9
Fred Banda <fredcbanda@hotmail.com>
2024-02-19 14:34:15 +0200

    escaped some stuff

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
           <div className="text-4xl pb-5 md:text-7xl px-6 text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to bg--neutral-400 bg-opacity-50">
             Partner with us <br /> for immeasurable growth!
           </div>
-          <p className="mt-4 text-lg font-normal text-neutral-400 max-w-2xl text-center mx-auto px-4">Since 2013, we've been dedicated to delivering top-tier software and web solutions, empowering businesses with cutting-edge technology. It all starts with The First Pitch.</p>
+          <p className="mt-4 text-lg font-normal text-neutral-400 max-w-2xl text-center mx-auto px-4">Since 2013, we&apos;ve been dedicated to delivering top-tier software and web solutions, empowering businesses with cutting-edge technology. It all starts with The First Pitch.</p>
           <Link href={"/book"}
           className="cursor-pointer justify-center flex items-center border rounded-full w-48 mx-auto my-6  h-12 animate-shimmer border-sky-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-sky-400 transition-colors focus:outline-none focus:ring-2 focus:ring-sky-400 focus:ring-offset-2 focus:ring-offset-sky-50"
           >
--- components/services/graphic-design.tsx
+++ components/services/graphic-design.tsx
@@ -15,7 +15,7 @@ const GraphicDesign = () => {
     <section className=" dark:bg-gray-500 rounded-lg bg-black/[0.10] antialiased bg-grid-white/[0.02]">
     <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
         <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text dark:text-white">We didn't reinvent the wheel</h2>
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text dark:text-white">We didn&apos;t reinvent the wheel</h2>
             <p className="mb-4">We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.</p>
             <p>We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick.</p>
         </div>
