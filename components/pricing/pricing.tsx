b6cacd494a06256425624dfd75ef94856b5b5aee
Fred Banda <fredcbanda@hotmail.com>
2024-02-21 20:02:03 +0200

    pricing edited

--- components/pricing/pricing.tsx
+++ components/pricing/pricing.tsx
@@ -3,164 +3,121 @@ import React from 'react'
 const Pricing = () => {
   return (
         <>
-    <div className="min-h-screen flex justify-center items-center mt-[-48px] mb-[88px]">
-        <div className="">
-            <div className="text-center font-semibold">
-                <h1 className="text-5xl">
-                    <span className="text-blue-700 tracking-wide">Flexible Plans To Grow </span>
-                    <span>Plans</span>
-                </h1>
-                <p className="pt-6 text-xl text-gray-400 font-normal px-8 md:w-full text-center w-full">
-                    Choose a plan that works best for you and your team.<br /> All the plans include one year domain registration but excludes hosting.
-                </p>
+        <div className="pt-5 bg-gray-900" id="pricing">
+        <div className="mx-auto pb-20 mt-4 max-w-7xl px-6 lg:px-8">
+            <div className="mx-auto max-w-4xl text-center">
+            <h1 className="text-base font-semibold leading-7 text-indigo-400">Pricing</h1>
+            <p className="mt-2 text-4xl font-bold tracking-tight text-white sm:text-5xl">For teams that are not scared of growth</p>
             </div>
-            <div className="pt-24 flex flex-row">
-                <div className="w-96 p-8 bg-white text-center rounded-3xl pr-16 shadow-xl">
-                    <h1 className="text-black font-semibold text-2xl">Startup</h1>
-                    <p className="pt-2 tracking-wide">
-                        <span className="text-gray-400 align-top">ZAR </span>
-                        <span className="text-3xl font-semibold">2990.00</span>
-                        <span className="text-gray-400 font-medium"></span>
-                    </p>
-                    <hr className="mt-4 border-1" />
-                    <div className="pt-8">
-                        <p className="font-semibold text-gray-400 text-left">
-                            <span className="material-icons align-middle">
-                                responsive
-                            </span>
-                            <span className="pl-2">
-                                Website with <span className="text-black">5 pages</span>
-                            </span>
-                        </p>
-                        <p className="font-semibold text-gray-400 text-left pt-5">
-                            <span className="material-icons align-middle">
-                                well
-                            </span>
-                            <span className="pl-2">
-                                researched <span className="text-black">5 keywords</span>
-                            </span>
-                        </p>
-                        <p className="font-semibold text-gray-400 text-left pt-5">
-                            <span className="material-icons align-middle">
-                                plus
-                            </span>
-                            <span className="pl-2">
-                                <span className="text-black">3 months of</span> free edits
-                            </span>
-                        </p>
+            <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-300">Choose the package that works best</p>
+            <div className="isolate mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
 
-                        <a href="#" className="">
-                            <p className="w-full py-4 bg-blue-600 mt-8 rounded-xl text-white">
-                                <span className="font-medium">
-                                    Get Started
-                                </span>
-                                <span className="pl-2 material-icons align-middle text-sm">
-                                    Now
-                                </span>
-                            </p>
-                        </a>
-                    </div>
+            <div className="ring-1 ring-white/10 rounded-3xl p-8 xl:p-10">
+                <div className="flex items-center justify-between gap-x-4">
+                <h2 id="product1" className="text-lg font-semibold leading-8 text-white ">Startup</h2>
                 </div>
+                <p className="mt-4 text-sm leading-6 text-gray-300">Perfect for that side hustle or just starting out.</p>
+                <p className="mt-6 flex items-baseline gap-x-1">
+                <span className="text-4xl font-bold tracking-tight text-white">ZRA 2999.00</span><span className="text-sm font-semibold leading-6 text-gray-300"></span>
+                </p>
 
-                <div className="w-80 p-8 bg-gray-900 text-center rounded-3xl text-white border-4 shadow-xl border-white transform scale-125">
-                    <h1 className="text-white font-semibold text-2xl">Growth Plan</h1>
-                    <p className="pt-2 tracking-wide">
-                        <span className="text-gray-400 align-top">ZAR </span>
-                        <span className="text-3xl font-semibold">3499.00</span>
-                        <span className="text-gray-400 font-medium"></span>
-                    </p>
-                    <hr className="mt-4 border-1 border-gray-600" />
-                    <div className="pt-8">
-                        <p className="font-semibold text-gray-400 text-left">
-                            <span className="material-icons align-middle">
-                                With
-                            </span>
-                            <span className="pl-2">
-                                All features in <span className="text-white">Startup</span>
-                            </span>
-                        </p>
-                        <p className="font-semibold text-gray-400 text-left pt-5">
-                            <span className="material-icons align-middle">
-                               Well
-                            </span>
-                            <span className="pl-2">
-                                Researched <span className="text-white">15 Keywords</span>
-                            </span>
-                        </p>
-                        <p className="font-semibold text-gray-400 text-left pt-5">
-                            <span className="material-icons align-middle">
-                        
-                            </span>
-                            <span className="pl-2">
-                                <span className="text-white">6 months</span> of free editing
-                            </span>
-                        </p>
+                <ul role="list" className="mt-8 space-y-3 text-sm leading-6 text-gray-300 xl:mt-10">
+                <li className="flex gap-x-3"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                    aria-hidden="true" className="h-6 w-5 flex-none text-white">
+                    <path fillRule="evenodd"
+                        d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                        clipRule="evenodd"></path>
+                    </svg>5 x Responsive pages</li>
+                <li className="flex gap-x-3"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                    aria-hidden="true" className="h-6 w-5 flex-none text-white">
+                    <path fillRule="evenodd"
+                        d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                        clipRule="evenodd"></path>
+                    </svg>5 Well Researched Keywords</li>
+                <li className="flex gap-x-3"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                    aria-hidden="true" className="h-6 w-5 flex-none text-white">
+                    <path fillRule="evenodd"
+                        d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                        clipRule="evenodd"></path>
+                    </svg>3 Months of free editing</li>
+                </ul>
+                <a href="/order" aria-describedby="product2"
+                className="bg-indigo-500 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline-indigo-500 mt-6 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2">
+                Get Started
+                </a>
+            </div>
 
-                        <a href="#" className="">
-                            <p className="w-full py-4 bg-blue-600 mt-8 rounded-xl text-white">
-                                <span className="font-medium">
-                                    Get Started
-                                </span>
-                                <span className="pl-2 material-icons align-middle text-sm">
-                                    Now
-                                </span>
-                            </p>
-                        </a>
-                    </div>
-                    <div className="absolute top-4 right-4">
-                        <p className="bg-blue-700 font-semibold px-4 py-1 rounded-full uppercase text-xs">Popular</p>
-                    </div>
+            <div className="bg-white/5 ring-2 ring-indigo-500 rounded-3xl p-8 xl:p-10">
+                <div className="flex items-baseline justify-between gap-x-4">
+                <h2 id="product2" className="text-lg font-semibold leading-8 text-white">Growthmate</h2>
+                <p className="rounded-full bg-indigo-500 px-2.5 py-1 text-xs font-semibold leading-5 text-white">Most popular</p>
                 </div>
+                <p className="mt-4 text-sm leading-6 text-gray-300">For businesses looking to expand their reach including but not limited to online stores</p>
+                <p className="mt-6 flex items-baseline gap-x-1">
+                <span className="text-4xl font-bold tracking-tight text-white">ZAR 3999.00</span><span className="text-sm font-semibold leading-6 text-gray-300"></span>
+                </p>
+                <ul role="list" className="mt-8 space-y-3 text-sm leading-6 text-gray-300 xl:mt-10">
+                <li className="flex gap-x-3"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                    aria-hidden="true" className="h-6 w-5 flex-none text-white">
+                    <path fillRule="evenodd"
+                        d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                        clipRule="evenodd"></path>
+                    </svg>Everything in Startup</li>
+                <li className="flex gap-x-3"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                    aria-hidden="true" className="h-6 w-5 flex-none text-white">
+                    <path fillRule="evenodd"
+                        d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                        clipRule="evenodd"></path>
+                    </svg>Well Researched 15 Keywords</li>
+                <li className="flex gap-x-3"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                    aria-hidden="true" className="h-6 w-5 flex-none text-white">
+                    <path fillRule="evenodd"
+                        d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                        clipRule="evenodd"></path>
+                    </svg>Whole 6 Months of free editing </li>
+                </ul>
+                <a href="/order" aria-describedby="product2"
+                className="bg-indigo-500 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline-indigo-500 mt-6 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2">
+                Get Started
+                </a>
+            </div>
 
-                <div className="w-96 p-8 bg-white text-center rounded-3xl pl-16 shadow-xl">
-                    <h1 className="text-black font-semibold text-2xl">Enterprise</h1>
-                    <p className="pt-2 tracking-wide">
-                        <span className="text-gray-400 align-top"> ZAR </span>
-                        <span className="text-3xl font-semibold">5999.00</span>
-                        <span className="text-gray-400 font-medium"></span>
-                    </p>
-                    <hr className="mt-4 border-1" />
-                    <div className="pt-8">
-                        <p className="font-semibold text-gray-400 text-left">
-                            <span className="material-icons align-middle">
-                                With
-                            </span>
-                            <span className="pl-2">
-                                All features in <span className="text-black">Growth Plan</span>
-                            </span>
-                        </p>
-                        <p className="font-semibold text-gray-400 text-left pt-5">
-                            <span className="material-icons align-middle">
-                               Our
-                            </span>
-                            <span className="pl-2">
-                                Growth <span className="text-black">oriented</span>
-                            </span>
-                        </p>
-                        <p className="font-semibold text-gray-400 text-left pt-5">
-                            <span className="material-icons align-middle">
-                                With
-                            </span>
-                            <span className="pl-2">
-                                <span className="text-black">Unlimited</span> Responsive Pages
-                            </span>
-                        </p>
-
-                        <a href="#" className="">
-                            <p className="w-full py-4 bg-blue-600 mt-8 rounded-xl text-white">
-                                <span className="font-medium">
-                                    Get Started
-                                </span>
-                                <span className="pl-2 material-icons align-middle text-sm">
-                                    Now
-                                </span>
-                            </p>
-                        </a>
-                    </div>
+            <div className="ring-1 ring-white/10 rounded-3xl p-8 xl:p-10">
+                <div className="flex items-center justify-between gap-x-4">
+                <h2 id="product3" className="text-lg font-semibold leading-8 text-white">Enterprise</h2>
                 </div>
+                <p className="mt-4 text-sm leading-6 text-gray-300">For those businesses looking for an upper hand over their competition</p>
+                <p className="mt-6 flex items-baseline gap-x-1">
+                <span className="text-4xl font-bold tracking-tight text-white">ZAR 5999.00</span><span className="text-sm font-semibold leading-6 text-gray-300"></span>
+                </p>
+                <ul role="list" className="mt-8 space-y-3 text-sm leading-6 text-gray-300 xl:mt-10">
+                <li className="flex gap-x-3"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                    aria-hidden="true" className="h-6 w-5 flex-none text-white">
+                    <path fillRule="evenodd"
+                        d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                        clipRule="evenodd"></path>
+                    </svg>Everything on Growthmate</li>
+                <li className="flex gap-x-3"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                    aria-hidden="true" className="h-6 w-5 flex-none text-white">
+                    <path fillRule="evenodd"
+                        d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                        clipRule="evenodd"></path>
+                    </svg>30 well researched Keywords</li>
+                <li className="flex gap-x-3"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                    aria-hidden="true" className="h-6 w-5 flex-none text-white">
+                    <path fillRule="evenodd"
+                        d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                        clipRule="evenodd"></path>
+                    </svg>12 months of free editing & support</li>
+                </ul>
+                <a href="/order" aria-describedby="product2"
+                className="bg-indigo-500 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline-indigo-500 mt-6 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2">
+                    Get Started
+                </a>
+            </div>
             </div>
         </div>
-    </div>
+        </div>
         </>
   )
 }
